5f4cbe9a5175e06a6c7f710f692981db677e6f4c
120356aa <yurlovandrew@gmail.com>
2020-11-21 12:21:24 -0800

    -b

--- api/users/usersRouter.js
+++ api/users/usersRouter.js
@@ -197,14 +197,14 @@ userRouter.post('/pay', async (req, res) => {
   const { card } = token;
 
   // console.log('here');
-  console.log('=====Body====');
-  console.log(body);
-  console.log('=====Email====');
-  console.log(body.user.email);
-  console.log('=====Token====');
-  console.log(token);
-  console.log('=====Name====');
-  console.log(body.user.name);
+  // console.log('=====Body====');
+  // console.log(body);
+  // console.log('=====Email====');
+  // console.log(body.user.email);
+  // console.log('=====Token====');
+  // console.log(token);
+  // console.log('=====Name====');
+  // console.log(body.user.name);
 
   try {
       const customer = await stripe.customers.create({
@@ -220,19 +220,19 @@ userRouter.post('/pay', async (req, res) => {
       // console.log(idempotencyKey);
       // console.log(customer);
 
-      // const key = v4();
-      // const charge = await stripe.charges.create({
-      //     amount: body.price * 100,
-      //     currency: "USD",
-      //     customer: customer.id,
-      //     receipt_email: body.user.email,
-      //     description: 'Purchase',
-      // }, { idempotencyKey: key });
+      const key = v4();
+      const charge = await stripe.charges.create({
+          amount: body.price * 100,
+          currency: "USD",
+          customer: customer.id,
+          receipt_email: body.user.email,
+          description: 'Purchase',
+      }, { idempotencyKey: key });
 
-      // if (charge) {
-      //   res.status(202).json(charge);
-      //   console.log(charge);
-      // } else res.status(500).json({ err: 'Could not process payment' });
+      if (charge) {
+        res.status(202).json(charge);
+        console.log(charge);
+      } else res.status(500).json({ err: 'Could not process payment' });
   } catch (err) { res.status(500).json({ err: 'Internal server error', err })};
 })
 
